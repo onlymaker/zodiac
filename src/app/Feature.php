54e943d4fcca143a0e46a52d7ef5e74b575df55e
jibo <jibo@outlook.com>
2019-11-22 11:34:18 +0800

    Update utm settings

--- src/app/Feature.php
+++ src/app/Feature.php
@@ -34,24 +34,25 @@ class Feature
         $i = 0;
         $height = 300;
         $size = $f3->get('ALBUM_SIZE');
+        $feature = $params['feature'];
         $tag = new Tag();
         $gallery = new JigMapper('gallery');
         $gallery->load(null, Sort::DEFAULT);
         while ($i < $size && !$gallery->dry()) {
             $tags = explode(',', $gallery['featured'] ?? '');
-            if ($tag->match($params['feature'], $tags)) {
+            if ($tag->match($feature, $tags)) {
                 $fields = $gallery->cast();
                 $fields['bias'] = $fields['width'] / $fields['height'] * $height;
                 $fields['grow'] = $fields['bias'];
                 $fields['bottom'] = sprintf('%.2f%%',$fields['height'] / $fields['width'] * 100);//转换为百分比
-                $fields['url'] .= ((strpos($fields['url'], '?', 1)) ? '&' : '?') . 'utm_source=site&utm_medium=album&utm_campaign=gallery';
+                $fields['url'] .= ((strpos($fields['url'], '?', 1)) ? '&' : '?') . 'utm_source=feature&utm_medium=' . $feature . '&utm_campaign=gallery';
                 $album[] = $fields;
                 $i++;
             }
             $gallery->next();
         }
         $f3->set('album', $album);
-        $f3->set('more', $tag->more($params['feature']));
+        $f3->set('more', $tag->more($feature));
         echo \Template::instance()->render('feature.html');
     }
 }
