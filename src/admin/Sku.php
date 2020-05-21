5f1ef89c605b3044465888b1efacdccad3e8381b
jibo <jibo@outlook.com>
2020-05-21 14:36:44 +0800

    List all the SKU

--- src/admin/Sku.php
+++ src/admin/Sku.php
@@ -10,16 +10,29 @@ class Sku extends Index
     function get(\Base $f3, array $params)
     {
         $results = [];
-        $sku = $f3->get('GET.sku') ?? 'sku';
+        $sku = $f3->get('GET.sku');
         $gallery = new JigMapper('gallery');
-        $gallery->load(['@sku=?', $sku], Sort::DEFAULT);
-        while (!$gallery->dry()) {
-            $results[] = $gallery->cast();
-            $gallery->next();
+        if ($sku) {
+            $gallery->load(['@sku=?', $sku], Sort::DEFAULT);
+            while (!$gallery->dry()) {
+                $results[] = $gallery->cast();
+                $gallery->next();
+            }
+            $f3->set('gallery', $results);
+            $f3->set('pageNo', 1);
+            $f3->set('pageCount', 1);
+            echo \Template::instance()->render('admin/index.html');
+        } else {
+            $gallery->load();
+            while (!$gallery->dry()) {
+                $results[] = $gallery['sku'];
+                $gallery->next();
+            }
+            $results = array_unique($results);
+            \sort($results);
+            foreach ($results as $result) {
+                echo "<pre>{$result}</pre>";
+            }
         }
-        $f3->set('gallery', $results);
-        $f3->set('pageNo', 1);
-        $f3->set('pageCount', 1);
-        echo \Template::instance()->render('admin/index.html');
     }
 }
